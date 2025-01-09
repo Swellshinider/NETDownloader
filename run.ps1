0226ca1b55d10b3f2de027538641c500b5cc41c2
Swellshinider <eduardoleal.contact@gmail.com>
2025-01-09 08:23:58 -0300

    add support for release configuration in run.ps1 with -r flag

--- run.ps1
+++ run.ps1
@@ -1,1 +1,11 @@
-dotnet run --project .\NETDownloader
+param (
+    [switch]$Release
+)
+
+if ($Release) {
+    dotnet build --configuration Release
+    dotnet run --project .\NETDownloader --configuration Release
+} else {
+    dotnet build --configuration Debug
+    dotnet run --project .\NETDownloader --configuration Debug
+}
