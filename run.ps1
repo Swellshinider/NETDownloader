857222c3cfa541428068690d4545f27f11b9f226
Swellshinider <eduardoleal.contact@gmail.com>
2025-01-22 09:21:42 -0300

    build before running

--- run.ps1
+++ run.ps1
@@ -5,4 +5,5 @@ param (
 # Configuration based on the -r flag
 $configuration = if ($r) { "Release" } else { "Debug" }
 
+dotnet build --configuration $configuration
 Start-Process -FilePath "dotnet" -ArgumentList "run --project .\NETDownloader\ --configuration $configuration"
